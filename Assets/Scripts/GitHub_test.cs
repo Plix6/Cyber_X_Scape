09f218a81426cd267ae636e8bae1a622be844c64
ThDrago <ross.allan974@gmail.com>
2023-12-04 11:13:05 +0100

    Importing assets

--- Assets/Scripts/GitHub_test.cs
+++ Assets/Scripts/GitHub_test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GitHub_tets : MonoBehaviour
+public class GitHub_test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

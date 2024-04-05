67394602e53eb46a7cc3562b0df6cb18e09dfbaf
Plix6 <olivier.bossavit@gmail.com>
2024-04-05 16:16:31 +0200

    Added indicators

---
+++ Assets/Scripts/IndicatorsColors.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorsColors : MonoBehaviour
+{
+    private Renderer[] renderers;
+
+    private Color[] indicatorColors = {
+        new (205 / 255f, 28 / 255f, 19/255f, 0.8f), //red
+        new (66 / 255f, 91 / 255f, 207 / 255f, 0.8f), //blue
+        new (63 / 255f, 193 / 255f, 76 / 255f, 0.8f), //green
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].material.color = indicatorColors[i%3];
+        }
+    }
+}

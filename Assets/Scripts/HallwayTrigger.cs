4b18bccd76dbd52199be917d0ed974d170b55da1
Plix6 <olivier.bossavit@gmail.com>
2024-04-15 11:47:12 +0200

    Added transition hallway

---
+++ Assets/Scripts/HallwayTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HallwayTrigger : MonoBehaviour
+{
+    [SerializeField] private bool isExit = false;
+    [SerializeField] private string newSceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isExit && other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(newSceneName);
+        }
+    }
+}
